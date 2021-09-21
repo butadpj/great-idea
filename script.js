d1fecf58d202370181c48b43a8ec8d580470dfa0
butadpj <butadpj@gmail.com>
2021-09-21 14:26:31 +0800

    Remove nav__area--hide class

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ const navArea = document.querySelector('.nav__area');
 const navToggle = () => {
   document.querySelector('.area__open').addEventListener('click', () => {
     navArea.classList.add('nav__area--show');
-    navArea.classList.remove('nav__area--hide');
   });
 
   document.querySelector('.area__close').addEventListener('click', () => {
     navArea.classList.remove('nav__area--show');
-    navArea.classList.add('nav__area--hide');
   });
 };
 
